4c9a970511e459f4f78171df39cfb07d96d040a1
Daniel Sollien <daniel.sollien@proton.me>
2025-08-13 14:46:07 +0200

    Add functionality for converting several files

--- convert.js
+++ convert.js
@@ -1,13 +1,23 @@
 import fs from 'fs';
 import { marked } from 'marked';
 
-const md = fs.readFileSync('index.md', 'utf-8');
-let html = marked(md);
 
-const today = new Date().toLocaleDateString("no-NO");
-fs.writeFileSync('index.html', `
+const mdFiles = ['index.md', "my_blog.md"];
+for (const mdFile of mdFiles) {
+  const md = fs.readFileSync(mdFile, 'utf-8'); // eller din egen fil
+  let html = `
 <html>
-  <head><meta charset="utf-8"><title>Daniel Berge Sollien</title></head>
-  <body>${html.replace(/{{Date}}/g, today)}</body>
+<head>
+  <meta charset="utf-8">
+  <title>Min nettside</title>
+</head>
+<body>
+${marked(md)}
+</body>
 </html>
-`);
+`;
+  html = html.replace(/(href="[^"]+)\.md"/gi, '$1.html"');
+  const outHtml = mdFile.replace(/\.md/g, ".html");
+  fs.writeFileSync(outHtml, html);
+  console.log(outHtml + ' generert');
+}
