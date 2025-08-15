17e3234b9e195b8d211b114806a8d201592123de
Daniel Sollien <daniel.sollien@proton.me>
2025-08-15 09:25:18 +0200

    Refactor module
    Split out functions when possible, and protect with main function

--- convert.js
+++ convert.js
@@ -1,20 +1,11 @@
-import fs, { write } from 'fs';
-import { marked } from 'marked';
-import { createHash } from 'crypto';
-
-
-const mdFiles = ['index.md', "index_english.md"];
-const today = new Date().toLocaleDateString("no-NO");
-for (const mdFile of mdFiles) {
-  const md = fs.readFileSync(mdFile, 'utf-8'); // eller din egen fil
-  // ...
-  if (!hashIdentical(mdFile, md)) {
-    writeHtml(md, mdFile);
-  }
-}
+import process from "process";
+import { pathToFileURL } from 'url';
+import fs from "fs";
+import { marked } from "marked";
+import { createHash } from "crypto";
 
 function writeHtml(md, mdFile) {
-  console.log("Writing.. ");
+  const today = new Date().toLocaleDateString("no-NO");
   let html = `
 <html>
 <head>
@@ -30,27 +21,55 @@ ${marked(md)}
   const outHtml = mdFile.replace(/\.md/g, ".html");
   html = html.replace(/{{Date}}/g, today);
   fs.writeFileSync(outHtml, html);
-  console.log(outHtml + ' generert');
+  console.log(outHtml + " generert");
 }
 
-function hashIdentical(mdFile, md) {
+function mdContentNotChanged(mdFile, md) {
   const hashFile = mdFile.replace(/\.md/, ".hash");
-  let writeHtml = true;
+  let sameHash = false;
   // Checking if a file with the hash of the file exists
   if (fs.existsSync(hashFile)) {
     const mdHash = createHash("md5").update(md).digest("hex");
-    console.log(mdHash);
-    const fileContent = fs.readFileSync(hashFile, 'utf-8');
+    const fileContent = fs.readFileSync(hashFile, "utf-8");
     if (fileContent === mdHash) {
       console.log("Contents are identical");
-      writeHtml = false;
+      sameHash = true;
     } else {
       console.log("The contents are not identical!");
       fs.writeFileSync(hashFile, mdHash);
     }
   } else {
-    console.log("No pre-generated hash")
+    console.log("No pre-generated hash");
 
-    return writeHtml;
+    return sameHash;
   }
+}
+
+function findMdFiles() {
+  const files = fs
+    .readdirSync(".")
+    .filter((word) => word.endsWith(".md") && word !== "README.md");
+  console.log(files);
+  return files;
+}
+
+export { findMdFiles, mdContentNotChanged, writeHtml };
+
+function main() {
+  for (const mdFile of findMdFiles()) {
+    const md = fs.readFileSync(mdFile, "utf-8"); // eller din egen fil
+    // ...
+    if (mdContentNotChanged(mdFile, md) === false) {
+      writeHtml(md, mdFile);
+    } else {
+      console.log("Not writing");
+    }
+  }
+}
+// Optional: run only if executed directly
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+  console.log("Running main");
+  main();
+}else{
+  console.log("This doesn't work");
 }
